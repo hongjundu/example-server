4654601db2c52a5ac81c5ccd20c70d7b1b727fab
duhongjun <hongjun.du.cn@gmail.com>
2020-02-23 09:57:04 +0800

    makefile fix

--- docs/init.sql
+++ docs/init.sql
@@ -6,8 +6,8 @@ flush privileges;
 
 
 CREATE TABLE casbin_rule (     p_type VARCHAR(100),     v0 VARCHAR(100),     v1 VARCHAR(100),     v2 VARCHAR(100) );
-INSERT INTO casbin_rule VALUES('p', 'user', 'data', 'read');
-INSERT INTO casbin_rule VALUES('p', 'admin', 'data', 'read');
-INSERT INTO casbin_rule VALUES('p', 'admin', 'data', 'write');
-INSERT INTO casbin_rule(p_type, v0, v1)  VALUES('g', 'duhj', 'user');
-INSERT INTO casbin_rule(p_type, v0, v1)  VALUES('g', 'admin', 'admin');
+INSERT INTO casbin_rule(p_type, v0, v1, v2) VALUES('p', 'user_role', 'data', 'read');
+INSERT INTO casbin_rule(p_type, v0, v1, v2)  VALUES('p', 'admin_role', 'data', 'read');
+INSERT INTO casbin_rule(p_type, v0, v1, v2)  VALUES('p', 'admin_role', 'data', 'write');
+INSERT INTO casbin_rule(p_type, v0, v1)  VALUES('g', 'duhj', 'user_role');
+INSERT INTO casbin_rule(p_type, v0, v1)  VALUES('g', 'admin', 'admin_role');
